1281da9e9423a03e232896b0afccfbd10b768ef6
Erica Portnoy <eportnoy@princeton.edu>
2013-11-09 15:02:38 -0500

    with serial

--- ohboymusic.py
+++ ohboymusic.py
@@ -1,41 +1,28 @@
 import pygame
-# import RPi.GPIO as GPIO
+import serial
 import time
 
-# GPIO.setmode(GPIO.BCM)
-pinnums = [17]
-previnputs = [False for a in pinnums]
+numpins = 6
 
-updownnum = 30
+previnputs = [False for a in range(0, numpins)]
+ser = serial.Serial('/dev/ttyACMO', 9600)
+
 fadeouttime = 500
-
-# for i in pinnums:
-	# GPIO.setup(i,GPIO.IN)
 
 pygame.mixer.init()
 
-sounds = []
-sounds.append(pygame.mixer.Sound("Piano.ff.C3.wav"))
+letters = ["a", "b", "c", "d", "e", "f"]
+sounds = [pygame.mixer.Sound("notes/"+letter+".wav") for letter in letters]
 
 def play(i):
-	sounds[i].play(loops=-1)
-
-def stop(i):
-	sounds[i].fadeout(fadeouttime)
-
-play(0)
-time.sleep(2)
-stop(0)
+    sounds[i].play(loops=0)
 
 while True:
-	# for i in pinnums:
-		# reading = GPIO.input(i)
-		# # prev = previnputs[i]
-		# if prev:
-		# 	if reading - prev > updownnum:
-		# 		play(i)
-		# 	elif prev - reading > updownnum:
-		# 		stop(i)
-		# previnputs[i] = reading
-	time.sleep(0.05)
-	print "d"
+    line = ser.readline()
+    for i in range(0, numpins):
+        curr = line[i] != '0'
+        prev = previnputs[i]
+        if curr and not prev:
+            play(i)
+            print i
+        previnputs[i] = curr
