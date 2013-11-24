dad05fd4584c0a35e93d081ae0fc8c1a05e9d042
Bonnie <bonnie.eisenman@gmail.com>
2013-11-24 16:30:39 -0500

    added continuous calibration

--- piano/piano.ino
+++ piano/piano.ino
@@ -8,8 +8,11 @@ void setup() {
 float analogPins[] = {0, 1, 2, 3, 4, 5};
 int numAnalogPins = 6;
 int thresholds[6];
-float percentThresh = 1.10;
+float percentThresh = 1.03;
 int absoluteThresh = 40;
+
+int sensorHistory[6][10];
+int bufferIndex = 0;
 
 void calibrate() {
   int maxSteps = 10;  
@@ -22,24 +25,47 @@ void calibrate() {
     thresholds[pin] = thresholds[pin] / maxSteps;
   }
   
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 10; j++) {
+       sensorHistory[i][j] = thresholds[i];
+    }
+  }
+  
 }
-
-int reCalibrate = 50;
-int loopCount = 0;
 
 void loop() {
   
   for (int i = 0; i < numAnalogPins; i++) {
     int val = analogRead(analogPins[i]);
     //Serial.println(val);
-    //if (val > thresholds[i] * percentThresh) {
-    if (val > thresholds[i] + absoluteThresh) {
-      Serial.print(1);
+    int oldAvg = 0;
+    for (int j = 0; j < 10; j++) {
+      oldAvg += sensorHistory[i][j];
+    }
+    oldAvg = oldAvg / 10;
+//    Serial.print(oldAvg);
+//    Serial.print(";");
+//    Serial.print(val);
+//    Serial.print(";");
+//    Serial.print(bufferIndex);
+//    Serial.println();
+    if (val > oldAvg + absoluteThresh) {
+       Serial.print(1);
     }
     else {
       Serial.print(0);
     }
+    sensorHistory[i][bufferIndex] = val;
+    //if (val > thresholds[i] * percentThresh) {
+//    if (val > thresholds[i] + absoluteThresh) {
+//      Serial.print(1);
+//    }
+//    
+//    else {
+//      Serial.print(0);
+//    }
    }
+  bufferIndex = (bufferIndex + 1) % 10;
   Serial.println();
-  delay(50);
+  delay(200);
 }
