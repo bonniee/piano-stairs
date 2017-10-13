d4750f3b6e3ccec379bf27b3192152ad5a52e047
Bonnie Eisenman <beisenman@twitter.com>
2017-10-13 17:40:20 -0400

    Comments

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Blog post:
 bonnie-eisenman.tumblr.com/post/66630942173/hackprinceton-fall-2013-piano-stairs
 
 Instructables: http://www.instructables.com/id/Piano-Stairs-with-Arduino-and-Raspberry-Pi/?ALLSTEPS
+
+# Code
+
+Run `ohboymusic.py` on your Raspberry Pi.
+
+Run `piano-sketch` on your Arduino.
--- piano-sketch/piano-sketch.ino
+++ piano-sketch/piano-sketch.ino
@@ -1,4 +1,11 @@
-
+/**
+ * piano-sketch.ino
+ * Author: Bonnie Eisenman
+ *
+ * Arduino-side code for taking in inputs from several sensors,
+ * and if they pass the thresholds, send information to
+ * Serial for processing.
+ **/
 
 void setup() {
   Serial.begin(9600);
@@ -8,47 +15,55 @@ void setup() {
 float analogPins[] = {0, 1, 2, 3, 4, 5};
 int numAnalogPins = 6;
 int thresholds[6];
-float percentThresh = 1.03;
-int absoluteThresh = 20;
 
+// These values can be tuned depending on sensor installation.
+float percentThresh = 1.03; // Not currently used
+int absoluteThresh = 20; // The actual threshold value used
+
+// Keep track of the last N x H sensor readings,
+// where N = number of inputs
+// and H = history length.
+// This allows us to do smoothing.
 int sensorHistory[6][10];
+
+// This is used to cycle through the previous array
+// along the H dimension.
 int bufferIndex = 0;
 
 void calibrate() {
-  int maxSteps = 10;  
+  // How many steps' worth of data we use for calibration purposes
+  int maxSteps = 10;
+
+  // Begin by accumulating several steps' worth of baseline data
   for (int curStep = 0; curStep < maxSteps; curStep++) {
     for (int pin = 0; pin < numAnalogPins; pin++) {
        thresholds[pin] += analogRead(analogPins[pin]);   
     }
   }
+
+  // Average out the baseline readings based on how many input readings we did
   for (int pin = 0; pin < numAnalogPins; pin++) {
     thresholds[pin] = thresholds[pin] / maxSteps;
   }
   
+  // Fill the entire sensorHistory array with baseline data
   for (int i = 0; i < 6; i++) {
     for (int j = 0; j < 10; j++) {
        sensorHistory[i][j] = thresholds[i];
     }
-  }
-  
+  } 
 }
 
 void loop() {
   
   for (int i = 0; i < numAnalogPins; i++) {
     int val = analogRead(analogPins[i]);
-    //Serial.println(val);
     int oldAvg = 0;
     for (int j = 0; j < 10; j++) {
       oldAvg += sensorHistory[i][j];
     }
     oldAvg = oldAvg / 10;
-//    Serial.print(oldAvg);
-//    Serial.print(";");
-//    Serial.print(val);
-//    Serial.print(";");
-//    Serial.print(bufferIndex);
-//    Serial.println();
+
     if (val > oldAvg + absoluteThresh) {
        Serial.print(1);
     }
@@ -56,14 +71,6 @@ void loop() {
       Serial.print(0);
     }
     sensorHistory[i][bufferIndex] = val;
-    //if (val > thresholds[i] * percentThresh) {
-//    if (val > thresholds[i] + absoluteThresh) {
-//      Serial.print(1);
-//    }
-//    
-//    else {
-//      Serial.print(0);
-//    }
    }
   bufferIndex = (bufferIndex + 1) % 10;
   Serial.println();
