eff5938c8844a5d057ea099702774cd3f6edf60a
Bonnie Eisenman <beisenman@twitter.com>
2017-10-13 17:26:35 -0400

    some changes

--- ohboymusic.py
+++ ohboymusic.py
@@ -3,7 +3,8 @@ import pygame
 import serial
 import time
 
-onpi = True
+# Switch this to false if debugging on a laptop
+READ_FROM_PI = True
 
 pygame.mixer.pre_init(channels=6, buffer=1024)
 pygame.mixer.init()
@@ -11,52 +12,40 @@ pygame.mixer.init()
 class PianoStairs():
 
     def __init__(self):
-        self.numpins = 6
-        # switch between piano and guitar every 3 minutes
-        self.seconds = 3 * 60
+        self.NUM_PINS = 6
 
-        self.previnputs = [False] * self.numpins
+        # Keep track of the previous values so that we can do smoothing
+        self.previnputs = [False] * self.NUM_PINS
 
-        if onpi == True:
+        if READ_FROM_PI == True:
             self.ser = serial.Serial('/dev/ttyACM0', 9600)
         letters = ["d", "e", "f", "g", "a", "b"]
         letters = letters[::-1]
         self.piano_notes = [pygame.mixer.Sound("piano-notes/"+letter+".wav") for letter in letters]
-        guit_let = ["e", "a", "d", "g", "b", "e2"]
-        self.guitar_notes = [pygame.mixer.Sound("guitar/"+letter+".wav") for letter in guit_let]
 
     def piano(self, i):
         self.piano_notes[i].play()
 
-    def guitar(self, i):
-        self.guitar_notes[i].play()
-
     def run(self):
-        count = 0
-        playguit = False
-
+        # Sleep while we wait for everything to boot up.
         time.sleep(3)
 
         while True:
-            count += 1
-            if count % (20 * self.seconds) == 0:
-                playguit = not playguit
             line = ""
-            if onpi:
+            if READ_FROM_PI:
                 line = self.ser.readline()
             else:
                 line = raw_input()
+
+            # Don't do anything if something weird happened w/ serial communication
             if len(line) < 6:
                 continue
 
-            for i in range(self.numpins):
+            for i in range(self.NUM_PINS):
                 curr = line[i] != '0'
                 prev = self.previnputs[i]
                 if curr and not prev:
-                    if playguit:
-                        self.guitar(i)
-                    else:
-                        self.piano(i)
+                    self.piano(i)
                 self.previnputs[i] = curr
 
 if __name__ == "__main__":
